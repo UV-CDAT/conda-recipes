688ccfd6d55baae724c5016fa1d93afb456a1811
muryanto1 <muryanto1@llnl.gov>
2020-07-23 10:55:41 -0700

    update cdat_metapackage.bash for cdat 8.2.1.rc1

--- cdat_metapackage.bash
+++ cdat_metapackage.bash
@@ -1,16 +1,28 @@
 #!/bin/bash -x
-export VERSION=8.2
-export CDAT_VERSION=8.2
-export CDAT_INFO_VERSION=8.2
-export CDMS_VERSION=3.1.4
-export CDTIME_VERSION=3.1.2
-export GENUTIL_VERSION=8.2
-export LIBNETCDF_VERSION=4.6.2
-export VERSION_EXTRA=""
-export BUILD=3
-export OPERATOR="=="
-export CDMS_OPERATOR="=="
-export PY3_VERSION="<"
-export LIBNETCDF_OPERATOR="=="
+export VERSION=8.2.1.rc1
+export CDAT_VERSION=8.2.1.rc1
+export CDAT_INFO_VERSION=8.2.1.rc1
+export CDTIME_VERSION=3.1.4.rc1
+export CDMS_VERSION=3.1.5.rc2
+export GENUTIL_VERSION=8.2.1.rc2
+export CDUTIL_VERSION=8.2.1.rc1
+export VTK_CDAT_VERSION=8.2.0.8.2.1.rc1
+#export LIBNETCDF_VERSION=
+export BUILD=0
+export OP="=="
+export CHANNELS="-c conda-forge/label/cdat_dev -c conda-forge -c cdat/label/cdat_dev"
+conda metapackage cdat ${CDAT_VERSION} \
+    ${CHANNELS} \
+    --build-number ${BUILD} --dependencies \
+    "cdat_info ${OP}${CDAT_INFO_VERSION}" \
+    "cdtime ${OP}${CDTIME_VERSION}" \
+    "cdms2 ${OP}${CDMS_VERSION}" \
+    "genutil ${OP}${GENUTIL_VERSION}" \
+    "vtk-cdat ${OP}${VTK_CDAT_VERSION}" \
+    "dv3d ${OP}${VERSION}" \
+    "vcs ${OP}${VERSION}" \
+    "wk ${OP}${VERSION}" \
+    "vcsaddons ${OP}${VERSION}" \
+    matplotlib basemap jupyter cdp output_viewer esmpy scipy ipywidgets notebook
 
-conda metapackage -c conda-forge -c cdat/label/v82 cdat ${CDAT_VERSION}${VERSION_EXTRA} --build-number ${BUILD} --dependencies "cdat_info ${OPERATOR}${CDAT_INFO_VERSION}" "distarray ${OPERATOR}2.12.2" "cdms2 ${CDMS_OPERATOR}${CDMS_VERSION}" "cdtime ${OPERATOR}${CDTIME_VERSION}" "cdutil ${OPERATOR}${VERSION}" "genutil ${OPERATOR}${GENUTIL_VERSION}" vtk-cdat "dv3d ${OPERATOR}${VERSION}" "vcs ${OPERATOR}${VERSION}" "vcsaddons ${OPERATOR}${VERSION}" "wk ${OPERATOR}${VERSION}" matplotlib basemap ipython jupyter nb_conda flake8 autopep8 spyder eofs windspharm cibots cdp output_viewer esmpy=7.1.0 scipy "ffmpeg >4" "libnetcdf ${LIBNETCDF_OPERATOR=}${LIBNETCDF_VERSION}"
+
